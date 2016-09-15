d851fcb6e26cf148c783d5b83db54f797c2cad96
rich-farr <scarletmanuka@gmail.com>
2016-09-15 17:03:46 +1200

    Get example todo component working

--- components/todoList.js
+++ components/todoList.js
@@ -3,16 +3,41 @@ import React, { Component } from 'react'
 const ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 
-class TodoList extends React.Component {
+var TodoList = React.createClass({
 
-  render () {
+  getInitialState: function() {
+    return {items: ['hello', 'world', 'click', 'me']};
+  },
+  handleAdd: function() {
+    var newItems =
+    this.state.items.concat([prompt('Enter some text')]);
+    this.setState({items: newItems});
+  },
+  handleRemove: function(i) {
+    var newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({items: newItems});
+  },
+  render: function() {
+    var items = this.state.items.map(function(item, i) {
+      return (
+        <div key={item} onClick={this.handleRemove.bind(this, i)}>
+        {item}
+        </div>
+      );
+    }.bind(this));
     return (
-      <div id="todoList">
-        <h1>Da list will go here</h1>
+      <div>
+      <button onClick={this.handleAdd}>Add Item</button>
+      <ReactCSSTransitionGroup
+      transitionName="example"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={300}>
+      {items}
+      </ReactCSSTransitionGroup>
       </div>
-    )
+    );
   }
-
-}
+})
 
 export default TodoList
